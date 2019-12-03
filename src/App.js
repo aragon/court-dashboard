81100f2117e0c7c7007903819ed671e99ed182e6
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-12-03 12:46:57 -0300

    Remove components && add logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react'
 
-import Dashboard from './components/Dashboard'
-
 function App() {
-  return (
-    <div>
-      {/* Set your component here */}
-      <Dashboard />
-    </div>
-  )
+  return <div>{/* Set your component here */}</div>
 }
 
 export default App
