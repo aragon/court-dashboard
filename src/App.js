b063eeb56ee95c8c83e169eddcba4575e854ebe4
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-12-06 18:05:59 -0300

    Add padding to main content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Header from './Header'
 import ErrorPage from './ErrorPage'
 
 import Dashboard from './components/Dashboard/Dashboard'
-
 import Disputes from './components/Disputes/Disputes'
 
 function App() {
@@ -27,13 +26,19 @@ function App() {
             `}
           >
             <Main>
-              <Redirect from="/" to="/dashboard" />
-              <Switch>
-                <Route path="/dashboard" component={Dashboard} />
-                <Route exact path="/tasks" render={() => <div>tasks</div>} />
-                <Route exact path="/disputes" component={Disputes} />
-                <Route component={ErrorPage} />
-              </Switch>
+              <div
+                css={`
+                  padding-bottom: 32px;
+                `}
+              >
+                <Redirect from="/" to="/dashboard" />
+                <Switch>
+                  <Route path="/dashboard" component={Dashboard} />
+                  <Route exact path="/tasks" render={() => <div>tasks</div>} />
+                  <Route exact path="/disputes" component={Disputes} />
+                  <Route component={ErrorPage} />
+                </Switch>
+              </div>
             </Main>
           </div>
         </div>
