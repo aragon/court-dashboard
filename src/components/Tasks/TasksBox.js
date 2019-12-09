0cb606c9665f28b89dc18250782682f52f7e83f9
Rodrigo Perez <rodrigopi1989@gmail.com>
2019-12-09 13:05:09 -0300

    Tasks view (#15)

---
+++ src/components/Tasks/TasksBox.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Box, GU, useViewport } from '@aragon/ui'
+import TaskAmounts from './TasksAmounts'
+
+const TasksBox = () => {
+  const { below } = useViewport()
+  const compactMode = below('medium')
+  const tasks = [
+    { status: 'active', amount: 34 },
+    { status: 'completed', amount: 21 },
+    { status: 'archived', amount: 67 },
+  ]
+
+  return (
+    <Box heading="Overview">
+      <div
+        css={`
+          /*
+            * translate3d() fixes an issue on recent Firefox versions where the
+            * scrollbar would briefly appear on top of everything (including the
+            * sidepanel overlay).
+            */
+          min-height: 112px;
+          transform: translate3d(0, 0, 0);
+          overflow-x: auto;
+        `}
+      >
+        <ul
+          css={`
+            list-style: none;
+            display: flex;
+            ${compactMode
+              ? `
+                    flex-direction: column;
+                    padding: ${1 * GU}px 0;
+                  `
+              : ''}
+          `}
+        >
+          {tasks.map(({ amount, status }) => (
+            <li
+              key={amount}
+              css={`
+                display: block;
+                min-width: ${20 * GU}px;
+                ${compactMode ? `margin-bottom: ${3 * GU}px;` : ''}
+                &:last-of-type {
+                  min-width: unset;
+                  margin-bottom: 0;
+                }
+              `}
+            >
+              <TaskAmounts amount={amount} status={status} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Box>
+  )
+}
+
+export default TasksBox
---
+++ src/components/Tasks/TasksTable.js
@@ -0,0 +1,154 @@
+import React, { useMemo, useState } from 'react'
+import {
+  compareAsc,
+  endOfDay,
+  format,
+  isWithinInterval,
+  startOfDay,
+} from 'date-fns'
+import {
+  ContextMenu,
+  DataView,
+  DropDown,
+  GU,
+  IconApps,
+  Link,
+  Text,
+  textStyle,
+  theme,
+  useViewport,
+} from '@aragon/ui'
+
+import LocalIdentityBadge from '../LocalIdentityBadge/LocalIdentityBadge'
+import TasksFilters from './TasksFilters'
+
+const INITIAL_DATE_RANGE = { start: null, end: null }
+
+const getFilteredTasks = ({ tasks, selectedDateRange }) => {
+  return tasks.filter(
+    ({ taskName, disputeId, priority, juror, dueDate }) =>
+      !selectedDateRange.start ||
+      !selectedDateRange.end ||
+      isWithinInterval(new Date(dueDate), {
+        start: startOfDay(selectedDateRange.start),
+        end: endOfDay(selectedDateRange.end),
+      })
+  )
+}
+
+const TaskTable = ({ tasks }) => {
+  const [selectedDateRange, setSelectedDateRange] = useState(INITIAL_DATE_RANGE)
+  const [page, setPage] = useState(0)
+  const { below } = useViewport()
+  const compactMode = below('medium')
+
+  const handleSelectedDateRangeChange = range => {
+    setPage(0)
+    setSelectedDateRange(range)
+  }
+
+  const filteredTasks = getFilteredTasks({
+    tasks,
+    selectedDateRange,
+  })
+
+  const sortedTasks = useMemo(
+    () =>
+      filteredTasks.sort(({ dueDate: dateLeft }, { dueDate: dateRight }) =>
+        // Sort by date ascending
+        compareAsc(dateLeft, dateRight)
+      ),
+    [filteredTasks]
+  )
+
+  return (
+    <DataView
+      page={page}
+      onPageChange={() => {}}
+      heading={
+        <>
+          <div
+            css={`
+              padding-bottom: ${2 * GU}px;
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+            `}
+          >
+            <div
+              css={`
+                color: ${theme.content};
+                ${textStyle('body1')};
+              `}
+            >
+              Upcoming tasks
+            </div>
+
+            <div css="text-align: right;">
+              <DropDown
+                placeholder={
+                  <div
+                    css={`
+                      display: flex;
+                    `}
+                  >
+                    <IconApps />
+                    <Text
+                      css={`
+                        margin-left: ${GU}px;
+                      `}
+                    >
+                      Actions
+                    </Text>
+                  </div>
+                }
+                header="Actions"
+                items={[]}
+                onChange={() => {}}
+                width="162px"
+              />
+            </div>
+          </div>
+          {!compactMode && (
+            <TasksFilters
+              dateRangeFilter={selectedDateRange}
+              onDateRangeChange={handleSelectedDateRangeChange}
+            />
+          )}
+        </>
+      }
+      fields={[
+        { label: 'Task', priority: 1 },
+        { label: 'Dispute', priority: 2 },
+        { label: 'Priority', priority: 3 },
+        { label: 'Assigned to juror', priority: 4 },
+        { label: 'Due date', priority: 5 },
+      ]}
+      entries={sortedTasks}
+      renderEntry={({ taskName, disputeId, priority, juror, dueDate }) => {
+        const formattedDate = format(dueDate, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")
+        const hoursAndSec = format(dueDate, 'HH:mm')
+        console.log('Hour ', hoursAndSec)
+        return [
+          <Text key={1}>{taskName}</Text>,
+          <Link key={2}>#{disputeId}</Link>,
+          <Text key={3}>{priority}</Text>,
+          <LocalIdentityBadge key={4} connectedAccount entity={juror} />,
+          <div key={5}>{`${format(
+            formattedDate,
+            'dd/MM/yy'
+          )} at ${hoursAndSec} - Term `}</div>,
+        ]
+      }}
+      renderEntryActions={() => (
+        <ContextMenu zIndex={1}>
+          {/* <ContextMenuItem  />
+          <ContextMenuItem /> */}
+        </ContextMenu>
+      )}
+      onSelectEntries={() => {}}
+    />
+  )
+}
+
+export default TaskTable
