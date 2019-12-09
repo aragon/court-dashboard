d67f3ca6ad4b532ecd1139fc788a98c89c3a3108
Rodrigo Perez <rodrigopi1989@gmail.com>
2019-12-09 13:28:33 -0300

    stats

---
+++ src/components/Dashboard/DashboardStats.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LatestActivity from './LatestActivity'
+import CourtStats from './CourtStats'
+
+const DashboardStats = () => {
+  return (
+    <>
+      <LatestActivity /> <CourtStats />
+    </>
+  )
+}
+
+export default DashboardStats
