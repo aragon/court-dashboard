1c83607add80622d24bc4ea72b03bc5192a91c12
Rodrigo Perez <rodrigopi1989@gmail.com>
2019-12-15 17:27:53 -0300

    graph server

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { createClient, Provider } from 'urql'
+import {
+  createClient,
+  Provider,
+  cacheExchange,
+  dedupExchange,
+  fetchExchange,
+} from 'urql'
+
+import { devtoolsExchange } from '@urql/devtools'
 
 const client = createClient({
-  url: 'http://localhost:5000/graphql',
+  url: 'http://127.0.0.1:8000/subgraphs/name/aragon/aragon-court-rpc',
+  exchanges: [dedupExchange, devtoolsExchange, cacheExchange, fetchExchange],
 })
 
 ReactDOM.render(
