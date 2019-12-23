b0b85569ac1f87baed8d362384a08786f7b61e96
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-12-23 16:17:57 -0500

    Add urql client and exchanges

--- src/index.js
+++ src/index.js
@@ -2,10 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { createClient, Provider } from 'urql'
+import {
+  createClient,
+  Provider,
+  cacheExchange,
+  debugExchange,
+  fetchExchange,
+  subscriptionExchange,
+} from 'urql'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+import { devtoolsExchange } from '@urql/devtools'
+
+const subscriptionClient = new SubscriptionClient(
+  'wss://api.thegraph.com/subgraphs/name/aragon/aragon-court-rinkeby',
+  {}
+)
 
 const client = createClient({
-  url: 'http://localhost:5000/graphql',
+  url: 'https://api.thegraph.com/subgraphs/name/aragon/aragon-court-rinkeby',
+  exchanges: [
+    debugExchange,
+    devtoolsExchange,
+    cacheExchange,
+    fetchExchange,
+    subscriptionExchange({
+      forwardSubscription: operation => subscriptionClient.request(operation),
+    }),
+  ],
 })
 
 ReactDOM.render(
