6b54e9e2a31ebdc2a91834af7d3a65d7917ff0b2
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-01-08 14:29:48 -0500

    Rename query

---
+++ src/queries/court.js
@@ -0,0 +1,35 @@
+import gql from 'graphql-tag'
+
+export const CourtConfig = gql`
+  subscription CourtConfig($id: ID!) {
+    courtConfig(id: $id) {
+      currentTerm
+      termDuration
+      feeToken {
+        name
+        symbol
+        decimals
+      }
+      anjToken {
+        name
+        symbol
+        decimals
+      }
+      jurorFee
+      draftFee
+      settleFee
+      evidenceTerms
+      commitTerms
+      revealTerms
+      appealTerms
+      appealConfirmationTerms
+      finalRoundReduction
+      firstRoundJurorsNumber
+      appealStepFactor
+      maxRegularAppealRounds
+      appealCollateralFactor
+      appealConfirmCollateralFactor
+      minActiveBalance
+    }
+  }
+`
