5e69e44f0a776f0f908049b0e65fcf2c171cc4ca
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-01-08 18:18:35 -0500

    Update env variables and  make rinkeby default chain

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,9 @@
-const DEFAULT_CHAIN_ID = 1337 // rpc
+const DEFAULT_CHAIN_ID = 4 // rinkeby
 const envVars = {
-  CHAIN_ID: process.env.CHAIN_ID || DEFAULT_CHAIN_ID,
+  CHAIN_ID: parseInt(process.env.REACT_APP_CHAIN_ID) || DEFAULT_CHAIN_ID, // TODO: Find a way to remove REACT_APP prefix
 }
+
+console.log(process.env)
 
 export default function environment(name) {
   const envVar = envVars[name]
