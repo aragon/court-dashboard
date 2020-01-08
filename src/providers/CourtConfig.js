fe866675a802e7ef6e544e3f072a47254d61284c
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-01-08 13:31:40 -0500

    Add web3 provider and court config provider

---
+++ src/providers/CourtConfig.js
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
+import { useSubscription } from 'urql'
+import { CourtConfig as CourtConfigSubscription } from '../queries/courtConfig'
+
+import environment from '../environment'
+import { getNetworkName } from '../web3-utils'
+import { networks } from '../networks'
+
+const CHAIN_ID = environment('CHAIN_ID')
+const CourtConfigContext = React.createContext()
+
+function CourtConfigProvider({ children }) {
+  const courtAddress = networks[getNetworkName(CHAIN_ID)].court
+
+  console.log(courtAddress)
+
+  const [result] = useSubscription({
+    query: CourtConfigSubscription,
+    variables: { id: courtAddress.toLocaleLowerCase() },
+  })
+
+  // TODO: handle possible errors
+  const courtConfig = result.data && result.data.courtConfig
+
+  return (
+    <CourtConfigContext.Provider value={courtConfig}>
+      {children}
+    </CourtConfigContext.Provider>
+  )
+}
+
+CourtConfigProvider.propTypes = {
+  children: PropTypes.node,
+}
+
+function useCourtConfig() {
+  return useContext(CourtConfigContext)
+}
+
+export { CourtConfigProvider, useCourtConfig }
--- src/providers/Web3.js
+++ src/providers/Web3.js
@@ -1,24 +1,85 @@
-import React, { useContext } from 'react'
+import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
-import { useWeb3React } from '@web3-react/core'
+import {
+  UnsupportedChainIdError,
+  Web3ReactProvider,
+  useWeb3React,
+} from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { providers as EthersProviders } from 'ethers'
+import { getNetworkName } from '../web3-utils'
+import environment from '../environment'
 
-const Web3Context = React.createContext()
+const { Web3Provider: EthersWeb3Provider } = EthersProviders
+const CHAIN_ID = environment('CHAIN_ID')
 
-function Web3Provider({ children }) {
-  // TODO: get account from web3
-  const web3React = useWeb3React()
-  console.log('web3', web3React)
+const WEB3_REACT_CONNECTORS = new Map([
+  ['injected', new InjectedConnector({ supportedChainIds: [CHAIN_ID] })],
+])
+
+export function useWeb3Connect() {
+  const web3ReactContext = useWeb3React()
+
+  const activate = useCallback(
+    async type => {
+      const connector = WEB3_REACT_CONNECTORS.get(type)
+      console.log('connector', connector)
+      if (connector) {
+        try {
+          await web3ReactContext.activate(connector, null, true)
+        } catch (err) {
+          const log = typeof window !== 'undefined' ? window.alert : console.log
+
+          if (err instanceof UnsupportedChainIdError) {
+            log(
+              `Unsupported chain: please connect to the network called ${getNetworkName(
+                CHAIN_ID
+              )} in your Ethereum Provider.`
+            )
+            return
+          }
+
+          log('Unknown error, please try again.')
+        }
+      }
+    },
+    [web3ReactContext]
+  )
+
+  const {
+    chainId,
+    account,
+    library: ethersProvider,
+    deactivate,
+  } = web3ReactContext
+
+  return {
+    account,
+    activate,
+    deactivate,
+    ethersProvider,
+    networkName: getNetworkName(chainId),
+    web3ReactContext,
+  }
+}
+
+export function useConnectedAccount() {
+  const web3ReactContext = useWeb3React()
+  return web3ReactContext.account
+}
+
+export function Web3ConnectProvider({ children }) {
+  const getLibrary = useCallback(
+    provider => new EthersWeb3Provider(provider),
+    []
+  )
   return (
-    <Web3Context.Provider value={web3React}>{children}</Web3Context.Provider>
+    <Web3ReactProvider getLibrary={getLibrary}>{children}</Web3ReactProvider>
   )
 }
 
-Web3Provider.propTypes = {
+Web3ConnectProvider.propTypes = {
   children: PropTypes.node,
 }
 
-function useConnectedAccount() {
-  return useContext(Web3Context)
-}
-
-export { Web3Provider, useConnectedAccount }
+export default Web3ConnectProvider
