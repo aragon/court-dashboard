2d6f8b58890ef5ffce83d49cabeabb7d84df34f8
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-01-10 09:49:10 -0500

    Reorganize lib

--- src/lib/web3.js
+++ src/lib/web3.js
@@ -1,5 +1,12 @@
+import { DEFAULT_CHAIN_NAME } from '../environment'
+
 import sha3 from 'js-sha3'
+export { sha3 }
 const { keccak_256: keccak256 } = sha3
+
+export function getFunctionSignature(func) {
+  return sha3(func).slice(0, 10)
+}
 
 function toChecksumAddress(address) {
   if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
@@ -33,3 +40,45 @@ export function addressesEqual(first, second) {
 }
 
 export const addressPattern = '(0x)?[0-9a-fA-F]{40}'
+
+/**
+ * Shorten an Ethereum address. `charsLength` allows to change the number of
+ * characters on both sides of the ellipsis.
+ *
+ * Examples:
+ *   shortenAddress('0x19731977931271')    // 0x1973…1271
+ *   shortenAddress('0x19731977931271', 2) // 0x19…71
+ *   shortenAddress('0x197319')            // 0x197319 (already short enough)
+ *
+ * @param {string} address The address to shorten
+ * @param {number} [charsLength=4] The number of characters to change on both sides of the ellipsis
+ * @returns {string} The shortened address
+ */
+export function shortenAddress(address, charsLength = 4) {
+  const prefixLength = 2 // "0x"
+  if (!address) {
+    return ''
+  }
+  if (address.length < charsLength * 2 + prefixLength) {
+    return address
+  }
+  return (
+    address.slice(0, charsLength + prefixLength) +
+    '…' +
+    address.slice(-charsLength)
+  )
+}
+
+export function getNetworkName(chainId) {
+  chainId = String(chainId)
+
+  if (chainId === '1') return 'mainnet'
+  if (chainId === '3') return 'ropsten'
+  if (chainId === '4') return 'rinkeby'
+
+  return DEFAULT_CHAIN_NAME
+}
+
+export function isLocalNetwork(chainId) {
+  return getNetworkName(chainId) === DEFAULT_CHAIN_NAME
+}
