f7f4541b24346ce02b04d1d5509d5c4f42b7e806
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-01-29 23:18:40 -0500

    Update src/components/Dashboard/AccountBanner.js
    Co-Authored-By: Pierre Bertet <hello@pierre.world>

--- src/components/Dashboard/AccountBanner.js
+++ src/components/Dashboard/AccountBanner.js
@@ -42,7 +42,7 @@ const getBannerAttributes = (
       return {
         icon: userIcon,
         iconBackground: theme.positive.alpha(0.2),
-        title: 'You are elegible to be drafted',
+        title: 'You are eligible to be drafted',
         titleColor: theme.positive,
         paragraph: 'You are eligible to be drafted starting from the next term',
         showTimer: true,
