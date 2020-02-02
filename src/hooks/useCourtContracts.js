9993cde3e157e1c20215dcc9543ff4cd501d7eec
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-02-02 15:07:44 -0300

    Fix merge issue

--- src/hooks/useCourtContracts.js
+++ src/hooks/useCourtContracts.js
@@ -1,38 +1,27 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 import { useCourtConfig } from '../providers/CourtConfig'
 import { CourtModuleType } from '../types/court-module-types'
 import { useContract } from '../web3-contracts'
 
+import aragonCourtAbi from '../abi/AragonCourt.json'
 import jurorRegistryAbi from '../abi/JurorRegistry.json'
 import tokenAbi from '../abi/ERC20.json'
 import disputeManagerAbi from '../abi/DisputeManager.json'
 import votingAbi from '../abi/CRVoting.json'
 
 import { getFunctionSignature } from '../lib/web3-utils'
+import {
+  hashVote,
+  getOutcomeFromCommitment,
+  getVoteId,
+  hashPassword,
+} from '../utils/crvoting-utils'
+import { getModuleAddress } from '../utils/court-utils'
 import { bigNum } from '../lib/math-utils'
-import { getVoteId } from '../utils/crvoting-utils'
 
 const ACTIVATE_SELECTOR = getFunctionSignature('activate(uint256)')
-const GAS_LIMIT = 500000 // Should be relative to every tx ?
-
-function useJurorRegistryContract() {
-  const { modules } = useCourtConfig()
-
-  const jurorRegistryModule = useMemo(
-    () =>
-      modules.find(
-        mod => CourtModuleType[mod.type] === CourtModuleType.JurorsRegistry
-      ),
-    [modules]
-  )
-
-  const jurorRegistryAddress = jurorRegistryModule
-    ? jurorRegistryModule.address
-    : null
-
-  return useContract(jurorRegistryAddress, jurorRegistryAbi)
-}
+const GAS_LIMIT = 900000 // Should be relative to every tx ?
 
 // ANJ contract
 function useANJTokenContract() {
@@ -43,19 +32,33 @@ function useANJTokenContract() {
   return useContract(anjTokenAddress, tokenAbi)
 }
 
+// Fee token contract
+function useFeeTokenContract() {
+  const { feeToken } = useCourtConfig()
+
+  const feeTokenAddress = feeToken ? feeToken.id : null
+
+  return useContract(feeTokenAddress, tokenAbi)
+}
+
 // Court contracts
 function useCourtContract(moduleType, abi) {
-  const { modules } = useCourtConfig()
+  const { id, modules } = useCourtConfig()
 
-  const courtModule = modules.find(
-    mod => CourtModuleType[mod.type] === moduleType
-  )
-
-  const contractAddress = courtModule ? courtModule.address : null
+  let contractAddress
+  if (moduleType === CourtModuleType.AragonCourt) {
+    contractAddress = id
+  } else {
+    contractAddress = getModuleAddress(modules, moduleType)
+  }
 
   return useContract(contractAddress, abi)
 }
 
+/**
+ * All ANJ interactions
+ * @returns {Object} all available functions around ANJ balances
+ */
 function useANJActions() {
   const jurorRegistryContract = useCourtContract(
     CourtModuleType.JurorsRegistry,
@@ -111,13 +114,23 @@ export function useCourtActions() {
   }
 }
 
+/**
+ * All dispute interactions
+ * @returns {Object} all available functions around a dispute
+ */
 export function useDisputeActions() {
   const disputeManagerContract = useCourtContract(
     CourtModuleType.DisputeManager,
     disputeManagerAbi
   )
-
   const votingContract = useCourtContract(CourtModuleType.Voting, votingAbi)
+
+  const aragonCourtContract = useCourtContract(
+    CourtModuleType.AragonCourt,
+    aragonCourtAbi
+  )
+
+  const feeTokenContract = useFeeTokenContract()
 
   // Draft jurors
   const draft = useCallback(
@@ -129,35 +142,45 @@ export function useDisputeActions() {
 
   // Commit
   const commit = useCallback(
-    (disputeId, roundId, commitment) => {
+    (disputeId, roundId, commitment, password) => {
       const voteId = getVoteId(disputeId, roundId)
-      return votingContract.commit(voteId, commitment)
+      const hashedCommitment = hashVote(commitment, password)
+
+      return votingContract.commit(voteId, hashedCommitment)
     },
     [votingContract]
   )
 
   // Reveal
   const reveal = useCallback(
-    (disputeId, roundId, voter, outcome, salt) => {
+    (disputeId, roundId, voter, commitment, salt) => {
       const voteId = getVoteId(disputeId, roundId)
-      return votingContract.reveal(voteId, voter, outcome, salt)
+      const outcome = getOutcomeFromCommitment(commitment, salt)
+
+      return votingContract.reveal(voteId, voter, outcome, hashPassword(salt))
     },
     [votingContract]
   )
 
   // Leak
   const leak = useCallback(
-    (disputeId, roundId, voter, outcome, salt) => {
-      const voteId = getVoteId(disputeId, roundId)
+    (voteId, voter, outcome, salt) => {
       return votingContract.leak(voteId, voter, outcome, salt)
     },
     [votingContract]
   )
 
+  const approveFeeDeposit = useCallback(
+    value => {
+      return feeTokenContract.approve(disputeManagerContract.address, value)
+    },
+    [disputeManagerContract, feeTokenContract]
+  )
+
   // Appeal round of dispute
   const appeal = useCallback(
     (disputeId, roundId, ruling) => {
-      return disputeManagerContract.appeal(disputeId, roundId, ruling, {
+      return disputeManagerContract.createAppeal(disputeId, roundId, ruling, {
         gasLimit: GAS_LIMIT,
       })
     },
@@ -174,11 +197,110 @@ export function useDisputeActions() {
     [disputeManagerContract]
   )
 
-  return { draft, commit, reveal, leak, appeal, confirmAppeal }
+  const executeRuling = useCallback(
+    disputeId => {
+      return aragonCourtContract.executeRuling(disputeId, {
+        gasLimit: GAS_LIMIT,
+      })
+    },
+    [aragonCourtContract]
+  )
+  return {
+    approveFeeDeposit,
+    draft,
+    commit,
+    reveal,
+    leak,
+    appeal,
+    confirmAppeal,
+    executeRuling,
+  }
+}
+
+/**
+ *
+ * @param {string} disputeId id of the dispute
+ * @param {string} roundId id of the round
+ * @returns {Object} appeal deposit and confirm appeal deposit amounts
+ */
+export function useAppealDeposits(disputeId, roundId) {
+  const [appealDeposits, setAppealDeposits] = useState([bigNum(0), bigNum(0)])
+
+  const disputeManagerContract = useCourtContract(
+    CourtModuleType.DisputeManager,
+    disputeManagerAbi
+  )
+
+  useEffect(() => {
+    const getNextRoundDetails = async () => {
+      if (!disputeManagerContract) return
+      const nextRound = await disputeManagerContract.getNextRoundDetails(
+        disputeId,
+        roundId
+      )
+
+      const appealDeposit = nextRound[6]
+      const confirmAppealDeposit = nextRound[7]
+      setAppealDeposits([appealDeposit, confirmAppealDeposit])
+    }
+
+    getNextRoundDetails()
+  }, [disputeId, disputeManagerContract, roundId])
+
+  return appealDeposits
+}
+
+export function useFeeBalanceOf(account) {
+  const [balance, setBalance] = useState(bigNum(0))
+
+  const feeTokenContract = useFeeTokenContract()
+
+  useEffect(() => {
+    const getFeeBalance = async () => {
+      if (!feeTokenContract) return
+
+      const balance = await feeTokenContract.balanceOf(account)
+      setBalance(balance)
+    }
+
+    getFeeBalance()
+  }, [account, feeTokenContract])
+
+  return balance
+}
+
+export function useAppealFeeAllowance(owner) {
+  const [allowance, setAllowance] = useState(bigNum(0))
+
+  const courtConfig = useCourtConfig()
+  const disputeManagerAddress = getModuleAddress(
+    courtConfig.modules,
+    CourtModuleType.DisputeManager
+  )
+  const feeTokenContract = useFeeTokenContract()
+
+  useEffect(() => {
+    const getFeeAllowance = async () => {
+      if (!feeTokenContract) return
+
+      const allowance = await feeTokenContract.allowance(
+        owner,
+        disputeManagerAddress
+      )
+      setAllowance(allowance)
+    }
+
+    getFeeAllowance()
+  }, [disputeManagerAddress, feeTokenContract, owner])
+
+  return allowance
 }
 
 export function useTotalActiveBalancePolling(termId) {
-  const jurorRegistryContract = useJurorRegistryContract()
+  const jurorRegistryContract = useCourtContract(
+    CourtModuleType.JurorsRegistry,
+    jurorRegistryAbi
+  )
   const [totalActiveBalance, setTotalActiveBalance] = useState(bigNum(-1))
 
   const timeoutId = useRef(null)
