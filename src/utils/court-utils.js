9e6ff66bd703fab90745732eb60a9e36e1469d42
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-02-02 20:16:49 -0300

    Move subscription and query hooks to single file

--- src/utils/court-utils.js
+++ src/utils/court-utils.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import { bigNum } from '../lib/math-utils'
-
 function getFirstTermDate(courtConfig) {
   const { terms } = courtConfig
   return terms[0].startTime * 1000
@@ -11,22 +8,4 @@ export function getTermStartTime(term, courtConfig) {
   const termMs = term * termDuration
 
   return getFirstTermDate(courtConfig) + termMs
-}
-
-export function getVoteId(disputeId, roundId) {
-  return bigNum(disputeId)
-    .shln(128)
-    .add(bigNum(roundId))
-=======
-function getFirstTermDate(courtConfig) {
-  const { terms } = courtConfig
-  return terms[0].startTime * 1000
-}
-
-export function getTermStartTime(term, courtConfig) {
-  const { termDuration } = courtConfig
-  const termMs = term * termDuration
-
-  return getFirstTermDate(courtConfig) + termMs
->>>>>>> c53446f876d61287c1f4d9a02ec1b8495b844091
 }
