26b0a07a5791de337cb29b8a3b08f84c578b12b0
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-02-02 22:47:33 -0300

    uncomment code

--- src/components/Dashboard/LatestActivity.js
+++ src/components/Dashboard/LatestActivity.js
@@ -1,72 +1,72 @@
-// import React from 'react'
-// import { Box, GU, IdentityBadge, textStyle, useTheme } from '@aragon/ui'
+import React from 'react'
+import { Box, GU, IdentityBadge, textStyle, useTheme } from '@aragon/ui'
 
-// import Stepper from '../Stepper'
-// import Step from '../Step'
+import Stepper from '../Stepper'
+import Step from '../Step'
 
-// import { latestActivity } from '../../mock-data'
+import { latestActivity } from '../../mock-data'
 
-// function LatestActivity() {
-//   const theme = useTheme()
+function LatestActivity() {
+  const theme = useTheme()
 
-//   return (
-//     <Box heading="latest activity" padding={0}>
-//       <Stepper
-//         lineColor="#FFCDC5"
-//         lineExtraHeight={10}
-//         lineTop={15}
-//         lineWidth={2}
-//         css={`
-//           padding: ${3 * GU}px 0;
-//         `}
-//       >
-//         {latestActivity.map((activity, index) => (
-//           <Step
-//             key={index}
-//             stepPoint={
-//               <div
-//                 css={`
-//                   border-radius: 50%;
-//                   border: 2px solid ${theme.accent.alpha(0.2)};
-//                   padding: ${1 * GU}px;
-//                 `}
-//               >
-//                 <span
-//                   css={`
-//                     display: block;
-//                     padding: ${0.5 * GU}px;
-//                     background: ${theme.accent};
-//                     border-radius: 50%;
-//                   `}
-//                 />
-//               </div>
-//             }
-//             content={
-//               <div
-//                 css={`
-//                   line-height: 2;
-//                 `}
-//               >
-//                 <IdentityBadge entity={activity.account} badgeOnly />
-//                 <div>
-//                   {activity.action}{' '}
-//                   <a href={activity.target.link}>{activity.target.label}</a>
-//                 </div>
-//                 <div
-//                   css={`
-//                     ${textStyle('body4')};
-//                     color: ${theme.contentSecondary};
-//                   `}
-//                 >
-//                   {activity.date}
-//                 </div>
-//               </div>
-//             }
-//           />
-//         ))}
-//       </Stepper>
-//     </Box>
-//   )
-// }
+  return (
+    <Box heading="latest activity" padding={0}>
+      <Stepper
+        lineColor="#FFCDC5"
+        lineExtraHeight={10}
+        lineTop={15}
+        lineWidth={2}
+        css={`
+          padding: ${3 * GU}px 0;
+        `}
+      >
+        {latestActivity.map((activity, index) => (
+          <Step
+            key={index}
+            stepPoint={
+              <div
+                css={`
+                  border-radius: 50%;
+                  border: 2px solid ${theme.accent.alpha(0.2)};
+                  padding: ${1 * GU}px;
+                `}
+              >
+                <span
+                  css={`
+                    display: block;
+                    padding: ${0.5 * GU}px;
+                    background: ${theme.accent};
+                    border-radius: 50%;
+                  `}
+                />
+              </div>
+            }
+            content={
+              <div
+                css={`
+                  line-height: 2;
+                `}
+              >
+                <IdentityBadge entity={activity.account} badgeOnly />
+                <div>
+                  {activity.action}{' '}
+                  <a href={activity.target.link}>{activity.target.label}</a>
+                </div>
+                <div
+                  css={`
+                    ${textStyle('body4')};
+                    color: ${theme.contentSecondary};
+                  `}
+                >
+                  {activity.date}
+                </div>
+              </div>
+            }
+          />
+        ))}
+      </Stepper>
+    </Box>
+  )
+}
 
-// export default LatestActivity
+export default LatestActivity
