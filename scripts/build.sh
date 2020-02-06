f7c22da0ef7e062a018a939ba3c6e8bb639e828d
Pierre Bertet <hello@pierre.world>
2020-02-06 21:19:46 +0000

    Move the npm build script into a separate shell script

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+
+enable_sentry='0'
+branch=$(git symbolic-ref --short -q HEAD)
+
+if [ "$branch" = 'development' ] || [ "$branch" = 'master' ]; then
+  enable_sentry='1'
+fi
+
+build=$(git log --pretty=format:'%h' -n 1)
+
+npm run sync-assets && REACT_APP_ENABLE_SENTRY=$enable_sentry REACT_APP_BUILD=$build npx react-app-rewired build
