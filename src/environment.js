f875f1f93858fe8d0a96affb307c0dcae5a66e36
Pierre Bertet <hello@pierre.world>
2020-02-06 14:16:35 +0000

    Sentry integration

--- src/environment.js
+++ src/environment.js
@@ -1,15 +1,24 @@
-const DEFAULT_CHAIN_ID = 4 // rinkeby
+// rinkeby
+const CHAIN_ID_DEFAULT = 4
 
-const envVars = {
-  CHAIN_ID: parseInt(process.env.REACT_APP_CHAIN_ID) || DEFAULT_CHAIN_ID, // TODO: Find a way to remove REACT_APP prefix
+// TODO: Find a way to remove REACT_APP prefix
+const ENV_VARS = {
+  CHAIN_ID: () => {
+    const chainId = parseInt(process.env.REACT_APP_CHAIN_ID)
+    return isNaN(chainId) ? CHAIN_ID_DEFAULT : chainId
+  },
+  SENTRY_DSN: () => {
+    const dsn = process.env.REACT_APP_SENTRY_DSN || ''
+    return dsn.trim()
+  },
+  BUILD: () => {
+    return process.env.REACT_APP_BUILD || 'undefined'
+  },
+}
+
+export default function environment(name) {
+  const envVar = ENV_VARS[name]
+  return typeof envVar === 'function' ? envVar() : null
 }
 
 export const DEFAULT_LOCAL_CHAIN = 'rpc'
-
-export default function environment(name) {
-  const envVar = envVars[name]
-  if (!envVar) {
-    return null
-  }
-  return envVar
-}
