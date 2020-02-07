21a3e8b0aea109f3d62291ec4e637c4a78a27f8f
Pierre Bertet <hello@pierre.world>
2020-02-07 00:01:44 +0000

    Trigger rebuild

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,8 +28,10 @@ echo "Build: $build"
 echo "Enable Sentry: $enable_sentry"
 echo ""
 
-echo "Building assets…"
+echo "Syncing assets…"
+echo ""
 npm run sync-assets
 
 echo "Building app…"
+echo ""
 REACT_APP_ENABLE_SENTRY=$enable_sentry REACT_APP_BUILD=$build npx react-app-rewired build
