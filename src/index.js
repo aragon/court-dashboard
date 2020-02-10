f52cede5a6cb48d41cca303675d26104cd00189b
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-02-09 21:45:27 -0300

    Graph reconnection attempts (#92)
    * Add  the graph connection errors handling
    
    * Add max reconnection attempts
    
    * Increase max attempts
    
    * PR comments

--- src/index.js
+++ src/index.js
@@ -18,7 +18,10 @@ import { getNetworkName } from './lib/web3-utils'
 
 const [GRAPH_API_ENDPOINT_HTTP, GRAPH_API_ENDPOINT_WS] = endpoints()
 
-const subscriptionClient = new SubscriptionClient(GRAPH_API_ENDPOINT_WS, {})
+const subscriptionClient = new SubscriptionClient(GRAPH_API_ENDPOINT_WS, {
+  reconnect: true,
+  reconnectionAttempts: 10,
+})
 
 const client = createClient({
   url: GRAPH_API_ENDPOINT_HTTP,
@@ -33,13 +36,30 @@ const client = createClient({
   ],
 })
 
-if (env('SENTRY_DSN') && env('ENABLE_SENTRY')) {
+const sentryEnabled = !!(env('SENTRY_DSN') && env('ENABLE_SENTRY'))
+
+if (sentryEnabled) {
   Sentry.init({
     dsn: env('SENTRY_DSN'),
     environment: getNetworkName(env('CHAIN_ID')),
     release: 'court-dashboard@' + env('BUILD'),
   })
 }
+
+let connectionAttempts = 0
+subscriptionClient.onConnected(() => (connectionAttempts = 0))
+
+// Check for connection errors and if reaches max attempts send error log to Sentry
+subscriptionClient.onError(err => {
+  const maxReconnectionAttempts = subscriptionClient.reconnectionAttempts
+
+  if (sentryEnabled && maxReconnectionAttempts === ++connectionAttempts) {
+    Sentry.captureMessage(
+      `Connection error, could not connect to ${err.target.url}`
+    )
+  }
+  console.log('Retrying connection...')
+})
 
 ReactDOM.render(
   <UrqlProvider value={client}>
