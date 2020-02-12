77179b3a7a730728e41ae95b6701e2dd995665f8
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-02-12 20:19:29 -0300

    Change court address for Rinkeby (#132)

--- src/networks.js
+++ src/networks.js
@@ -1,6 +1,6 @@
 export const networks = {
   rpc: { court: '0xC89Ce4735882C9F0f0FE26686c53074E09B0D550' },
   ropsten: { court: '0x3b26bc496aebaed5b3E0E81cDE6B582CDe71396e' },
-  rinkeby: { court: '0x014e190d7ae3ed129baca6b72568c4b8555758c8' },
+  rinkeby: { court: '0xb5ffbe75fa785725eea5f931b64fc04e516c9c5d' },
   mainnet: { court: '0xee4650cBe7a2B23701D416f58b41D8B76b617797' },
 }
