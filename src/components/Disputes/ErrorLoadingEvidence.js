0c17ef95c6a0e43f73ce61655be342d6cb4dc222
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-02-20 18:32:19 -0300

    Update src/components/Disputes/ErrorLoadingEvidence.js
    Co-Authored-By: Pierre Bertet <hello@pierre.world>

--- src/components/Disputes/ErrorLoadingEvidence.js
+++ src/components/Disputes/ErrorLoadingEvidence.js
@@ -39,7 +39,7 @@ export default function ErrorLoadingEvidence() {
             ${textStyle('title2')}
           `}
         >
-          Oops, we couldn't load this piece of evidence
+          Oops, we couldn’t load this piece of evidence
         </span>
         <div
           css={`
