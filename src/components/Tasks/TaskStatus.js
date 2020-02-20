d8f01e1f3a98190ee47e5506d1a48134ac53f3bc
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-02-20 18:19:09 -0300

    due date fix

--- src/components/Tasks/TaskStatus.js
+++ src/components/Tasks/TaskStatus.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import { textStyle, GU } from '@aragon/ui'
+import dayjs from 'dayjs'
+import { datesDiff } from '../../utils/date-utils'
 
-function TaskStatus() {
+function getEndDay(dueDate) {
+  const diff = datesDiff(dayjs(), dayjs(dueDate))
+
+  if (diff === 0) {
+    return ' Due Today'
+  }
+  if (diff === 1) {
+    return 'In the next day'
+  }
+  return `In ${diff} days`
+}
+
+const TaskStatus = React.memo(function TaskStatus({ dueDate }) {
   return (
     <div css="display: flex; align-items:center;">
       <div css="width: 15px; height:15px; border-radius:2px; border: 1.5px solid #F5A623;" />
@@ -11,10 +25,10 @@ function TaskStatus() {
         margin-left: ${GU * 0.7}px;
       `}
       >
-        Open: Due today
+        {`Open: ${getEndDay(dueDate)}`}
       </span>
     </div>
   )
-}
+})
 
 export default TaskStatus
--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -8,3 +8,24 @@ const KNOWN_FORMATS = {
 export function dateFormat(date, format) {
   return dayjs(date).format(KNOWN_FORMATS[format] || format)
 }
+
+export function datesDiff(startDate, endDate) {
+  const endDateOfMonth = endDate.get('date')
+  const endMonth = endDate.get('month')
+  const endYear = endDate.get('year')
+  const currentDateOfMonth = startDate.get('date')
+  const currentMonth = startDate.get('month')
+  const currentEndYear = startDate.get('year')
+
+  if (
+    endDateOfMonth === currentDateOfMonth &&
+    endMonth === currentMonth &&
+    endYear === currentEndYear
+  ) {
+    return 0
+  }
+  if (endDate.diff(startDate, 'hour', true) < 24) {
+    return 1
+  }
+  return endDate.diff(startDate, 'day')
+}
