3eba78ee7f2bd5f62a435334520c9d54825a1f59
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-02-25 00:29:57 -0300

    error handler

---
+++ src/GlobalErrorHandler.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import * as Sentry from '@sentry/browser'
+import GenericError from './components/GenericError'
+// import { DAONotFound } from './errors'
+import env from './environment'
+import GlobalErrorScreen from './components/GlobalErrorScreen'
+import { getNetworkName } from './lib/web3-utils'
+
+const SENTRY_DSN = env('SENTRY_DNS')
+
+class GlobalErrorHandler extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+  }
+
+  state = { error: null, errorStack: null }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error,
+      errorStack: errorInfo.componentStack
+        .replace(/^\n+|\n+$/g, '')
+        .replace(/^ {4}/gm, ''),
+    })
+
+    // Once this point is reached, the app can not recover because the routing
+    // system, being below this component in the tree, is not functional
+    // anymore. To make hash changes work despite this (e.g. by pressing the
+    // back button in the browser), the page need to be reloaded.
+    window.removeEventListener('hashchange', this.handleHashchange)
+    window.addEventListener('hashchange', this.handleHashchange)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('hashchange', this.handleHashchange)
+  }
+
+  handleReportClick = () => {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      environment: getNetworkName(env('CHAIN_ID')),
+    })
+    const eventId = Sentry.captureException(this.state.error)
+    Sentry.showReportDialog({ eventId })
+  }
+
+  handleHashchange = () => {
+    window.location.reload()
+  }
+
+  render() {
+    const { children } = this.props
+    const { error, errorStack } = this.state
+
+    return error ? (
+      <GlobalErrorScreen>
+        <GenericError
+          detailsTitle={error.message}
+          detailsContent={errorStack}
+          reportCallback={SENTRY_DSN ? this.handleReportClick : null}
+        />
+      </GlobalErrorScreen>
+    ) : (
+      children
+    )
+  }
+}
+
+export default GlobalErrorHandler
---
+++ src/components/GlobalErrorScreen.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Card, GU, useTheme, useViewport } from '@aragon/ui'
+import globalError from '../assets/globalError.svg'
+
+function GlobalErrorScreen({ children }) {
+  return (
+    <div
+      css={`
+        height: 100vh;
+        min-width: ${45 * GU}px;
+        overflow: auto;
+      `}
+    >
+      <div
+        css={`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: ${8 * GU}px;
+          min-height: 100%;
+        `}
+      >
+        <Container>
+          <img
+            src={globalError}
+            alt=""
+            width="147"
+            height="144"
+            css={`
+              display: block;
+              margin: ${5 * GU}px auto ${1.5 * GU}px;
+            `}
+          />
+          {children}
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+GlobalErrorScreen.propTypes = {
+  children: PropTypes.node,
+}
+
+function Container({ children }) {
+  const theme = useTheme()
+  const { width } = useViewport()
+  return width < 60 * GU ? (
+    <div
+      css={`
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        min-width: ${45 * GU}px;
+        overflow: auto;
+        background: ${theme.surface};
+        display: grid;
+        align-items: center;
+      `}
+    >
+      <div
+        css={`
+          padding: ${5 * GU}px ${6 * GU}px ${6 * GU}px;
+        `}
+      >
+        {children}
+      </div>
+    </div>
+  ) : (
+    <Card
+      css={`
+        display: block;
+        padding: ${5 * GU}px ${6 * GU}px;
+        width: 100%;
+        max-width: ${72 * GU}px;
+        height: auto;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+        cursor: unset;
+      `}
+    >
+      {children}
+    </Card>
+  )
+}
+
+Container.propTypes = {
+  children: PropTypes.node,
+}
+
+export default GlobalErrorScreen
