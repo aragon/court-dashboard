250cc09773958295d8b21a5940971300d9bfbb2d
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-03-04 17:35:59 -0300

    box

--- src/components/Dashboard/CourtStats.js
+++ src/components/Dashboard/CourtStats.js
@@ -1,18 +1,82 @@
 import React from 'react'
-import { Box, Distribution } from '@aragon/ui'
+import { Box, GU, textStyle, useTheme } from '@aragon/ui'
+import IconANJ from '../../assets/IconANJ.svg'
+// import { formatUnits, bigNum } from '../../lib/math-utils'
+// import LatestActivity from './LatestActivity'
+// import CourtStats from './CourtStats'
+
+// const splitAmount = amount => {
+//   const [integer, fractional] = amount.split('.')
+//   return (
+//     <span
+//       css={`
+//         margin-right: 5px;
+//       `}
+//     >
+//       <span className="integer">{integer}</span>
+//       {fractional && (
+//         <span
+//           css={`
+//             font-size: 16px;
+//           `}
+//         >
+//           .{fractional}
+//         </span>
+//       )}
+//     </span>
+//   )
+// }
 
 const CourtStats = () => {
+  const theme = useTheme()
+  const stats = {
+    title: 'Total Active ANJ',
+    token: {
+      symbol: 'ANJ',
+      value: 101490826,
+      decimals: 18,
+      icon: IconANJ,
+      usdValue: 82501892,
+    },
+  }
+
   return (
-    <Box heading="Court Stats">
-      <Distribution
-        heading="Disputes"
-        items={[
-          { item: 'New', percentage: 37 },
-          { item: 'Adjudicated', percentage: 22 },
-          { item: 'Appeals', percentage: 15 },
-          { item: 'Executed', percentage: 12 },
-        ]}
-      />
+    <Box heading="Court Metrics" padding={3 * GU}>
+      <span
+        css={`      
+          ${textStyle('body2')}
+          color: ${theme.surfaceContentSecondary};
+          display:block;
+          margin-bottom:${1 * GU}px;
+        `}
+      >
+        {stats.title}
+      </span>
+      <div>
+        <span
+          css={`
+            ${textStyle('title2')}
+            font-weight: 300;
+          `}
+        >
+          {stats.token.value}
+        </span>
+        <img
+          css={`
+            margin-right: 5px;
+          `}
+          height="20"
+          width="18"
+          src={stats.token.icon}
+          alt="ANJ"
+        />
+        <span
+          css={` ${textStyle('body2')}
+          color: ${theme.surfaceContentSecondary};`}
+        >
+          {stats.token.symbol}
+        </span>
+      </div>
     </Box>
   )
 }
