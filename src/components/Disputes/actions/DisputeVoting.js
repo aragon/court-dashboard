a466e39dd52c59ed84610edf06d9faa6ac641b60
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-04-07 10:28:28 -0300

    Add Refuse to vote hint (#245)
    * Add Refuse to vote hint
    
    * relocate hint
    
    * Dispute detail: Make responsive
    
    * Do PR comments
    
    * Update src/components/Disputes/actions/DisputeVoting.js
    
    Co-Authored-By: Brett Sun <qisheng.brett.sun@gmail.com>
    
    * Use styled components as prop
    
    Co-authored-by: Brett Sun <qisheng.brett.sun@gmail.com>

--- src/components/Disputes/actions/DisputeVoting.js
+++ src/components/Disputes/actions/DisputeVoting.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button, GU, Info } from '@aragon/ui'
+import { Button, GU, Help, Info, useTheme, useViewport } from '@aragon/ui'
 import { useActiveBalanceOfAt } from '../../../hooks/useCourtContracts'
 import { useCourtConfig } from '../../../providers/CourtConfig'
 import { useWallet } from '../../../providers/Wallet'
@@ -53,6 +53,10 @@ function VotingFinalRound({ draftTermId, onRequestCommit }) {
 
 function VotingActions({ canJurorVote, onRequestCommit }) {
   const wallet = useWallet()
+  const { below } = useViewport()
+  const compactMode = below('medium')
+
+  const buttonWidth = compactMode ? '100% ' : `calc((100% - ${2 * GU}px) /  3)`
 
   return (
     <div>
@@ -60,34 +64,40 @@ function VotingActions({ canJurorVote, onRequestCommit }) {
         css={`
           display: flex;
           justify-content: space-between;
+          flex-direction: ${compactMode ? 'column' : 'row'};
           width: 100%;
           margin-bottom: ${1.5 * GU}px;
         `}
       >
         <VotingButton
-          mode="positive"
-          wide
+          compactMode={compactMode}
           disabled={!canJurorVote}
+          mode="positive"
           onClick={() => onRequestCommit(VOTE_OPTION_IN_FAVOR)}
+          width={buttonWidth}
         >
           Allow action
         </VotingButton>
         <VotingButton
-          mode="negative"
-          wide
+          compactMode={compactMode}
           disabled={!canJurorVote}
+          mode="negative"
           onClick={() => onRequestCommit(VOTE_OPTION_AGAINST)}
+          width={buttonWidth}
         >
           Block action
         </VotingButton>
+
         <VotingButton
-          wide
+          compactMode={compactMode}
           disabled={!canJurorVote}
           onClick={() => onRequestCommit(VOTE_OPTION_REFUSE)}
+          width={buttonWidth}
         >
           Refuse to vote
         </VotingButton>
       </div>
+      <RefuseToVoteHint compactMode={compactMode} width={buttonWidth} />
       <Info mode={canJurorVote ? 'description' : 'warning'}>
         {(() => {
           if (!wallet.account)
@@ -102,8 +112,60 @@ function VotingActions({ canJurorVote, onRequestCommit }) {
   )
 }
 
+const RefuseToVoteHint = ({ compactMode, width }) => {
+  const theme = useTheme()
+
+  return (
+    <div
+      css={`
+        display: flex;
+        align-items: center;
+        justify-content: ${compactMode ? 'center' : 'flex-end'};
+        margin-bottom: ${1.5 * GU}px;
+      `}
+    >
+      <div
+        css={`
+          width: ${width};
+          color: ${theme.help};
+        `}
+      >
+        <Container as={compactMode ? 'label' : 'div'} compactMode={compactMode}>
+          <span
+            css={`
+              margin-right: ${0.5 * GU}px;
+            `}
+          >
+            Why refuse to vote?
+          </span>
+          <Help hint="">
+            <p>
+              You can refuse to vote for many reasons, for example if you
+              consider that the evidence was not conclusive enough or the
+              description was incoherent.
+            </p>
+            <p>
+              Remember that you should vote the way that you think the majority
+              of jurors will vote, since you will be penalized if your vote is
+              in the minority.
+            </p>
+          </Help>
+        </Container>
+      </div>
+    </div>
+  )
+}
+
 const VotingButton = styled(Button)`
-  width: calc((100% - ${2 * GU}px) / 3);
+  width: ${({ width }) => width};
+  margin-bottom: ${({ compactMode }) => (compactMode ? 1 : 0) * GU}px;
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: ${({ compactMode }) => (compactMode ? 'pointer' : '')};
 `
 
 export default DisputeVoting
