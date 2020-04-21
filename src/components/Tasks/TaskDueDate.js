431a0a884bdfee9b02ce5fac5f06d1ed20d6e0b7
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-04-21 16:35:36 -0300

    Tasks: Due date calculation and timer (#287)
    * Task Status
    Change the diff calculation between due date and now
    
    * Task Timer
     Not show minutes if the day dif is more that 1 day

--- src/components/Tasks/TaskDueDate.js
+++ src/components/Tasks/TaskDueDate.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Timer } from '@aragon/ui'
+import { dayjs } from '../../utils/date-utils'
 
 function TaskDueDate({ dueDate }) {
-  return <Timer end={new Date(dueDate)} />
+  const diff = dayjs(dueDate).diff(dayjs().startOf('day'), 'day')
+  return <Timer end={new Date(dueDate)} maxUnits={diff === 0 ? 3 : 2} />
 }
 
 export default TaskDueDate
