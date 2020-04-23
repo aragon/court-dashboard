69b220bcea00c31ec983aae2e908b6274ea49a3c
Brett Sun <qisheng.brett.sun@gmail.com>
2020-04-23 11:12:07 +0200

    chore: remove git from build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,28 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-enable_sentry='0'
-
-# Current branch
-if [ -n "$NOW_GITHUB_COMMIT_REF" ]; then
-  # ZEIT Now (no .git but an env var)
-  branch=$NOW_GITHUB_COMMIT_REF
-else
-  # Other environments
-  branch=$(git symbolic-ref --short -q HEAD)
-fi
-
-if [ "$branch" = 'development' ] || [ "$branch" = 'master' ]; then
-  enable_sentry='1'
-fi
-
-# Build number from the short hash
-if [ -n "$NOW_GITHUB_COMMIT_SHA" ]; then
-  build=$(echo "$NOW_GITHUB_COMMIT_SHA" | cut -c1-7)
-else
-  build=$(git log --pretty=format:'%h' -n 1)
-fi
-
 echo "Branch: $branch"
 echo "Build: $build"
 echo "Enable Sentry: $enable_sentry"
