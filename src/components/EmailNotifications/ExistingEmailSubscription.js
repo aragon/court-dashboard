a231d442cba08299a2b53fb14c4cdb6ad0bc19af
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-05-13 18:02:30 -0300

    Notifications: Existing email modal (#311)
    * UI: Existing email modal mode
    
    * Copy change
    
    * PR comments
    
    * Pr comments
    
    * Change logic to display the IdentityBadge
    
    * Use useWallet

---
+++ src/components/EmailNotifications/ExistingEmailSubscription.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Button, GU, Link, textStyle, useTheme } from '@aragon/ui'
+import { useWallet } from '../../providers/Wallet'
+import IdentityBadge from '../IdentityBadge'
+
+import emailNotifcationIllustration from '../../../src/assets/emailNotifications.svg'
+
+function ExistingEmailSubscription({
+  compactMode,
+  onOptOut,
+  onSubscribeToNotifications,
+}) {
+  const theme = useTheme()
+  const { account } = useWallet()
+
+  return (
+    <>
+      <div
+        css={`
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          padding-top: ${3 * GU}px;
+        `}
+      >
+        <div
+          css={`
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+          `}
+        >
+          <div
+            css={`
+              align-items: center;
+            `}
+          >
+            <img
+              src={emailNotifcationIllustration}
+              width={141}
+              height={141}
+              alt=""
+            />
+          </div>
+          <h3
+            css={`
+              ${textStyle('title2')};
+              margin-top: ${4 * GU}px;
+            `}
+          >
+            Stay up to date with email notifications
+          </h3>
+
+          <span
+            css={`
+              ${textStyle('body2')};
+              color: ${theme.surfaceContentSecondary};
+              margin-top: ${1.5 * GU}px;
+            `}
+          >
+            <span>We’ve detected an email associated to the account </span>
+            <IdentityBadge
+              connectedAccount={account}
+              entity={account}
+              compact
+            />
+            <span>
+              . First, we need to verify that you are in control of this account
+              and then, authenticate the email address you provided.
+            </span>
+          </span>
+        </div>
+        <span
+          css={`
+            ${textStyle('body2')};
+            color: #9096b6;
+            text-align: center;
+            margin-top: ${3 * GU}px;
+          `}
+        >
+          You have previously agreed to Aragon Court's{' '}
+          <Link href="https://anj.aragon.org/legal/terms-general.pdf">
+            legal terms
+          </Link>{' '}
+          and{' '}
+          <Link href="https://aragon.one/email-collection.md">
+            email collection policy
+          </Link>
+          .
+        </span>
+        <div
+          css={`
+            display: flex;
+            justify-content: space-between;
+            flex-direction: ${compactMode ? 'column' : 'row'};
+            width: 100%;
+            margin-bottom: ${1.5 * GU}px;
+            margin-top: ${3 * GU}px;
+          `}
+        >
+          <ActionButtons compactMode={compactMode} onClick={onOptOut}>
+            Opt out
+          </ActionButtons>
+          <ActionButtons
+            compactMode={compactMode}
+            mode="strong"
+            onClick={onSubscribeToNotifications}
+          >
+            Subscribe to notifications
+          </ActionButtons>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const ActionButtons = styled(Button)`
+  width: ${({ compactMode }) =>
+    compactMode ? '100% ' : `calc((100% - ${2 * GU}px) /  2)`};
+`
+
+export default ExistingEmailSubscription
