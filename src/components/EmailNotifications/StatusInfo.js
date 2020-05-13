4659af98f0974f28ac2bc0b2b6321ae37692194b
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-05-13 18:26:40 -0300

    PR comments

--- src/components/EmailNotifications/StatusInfo.js
+++ src/components/EmailNotifications/StatusInfo.js
@@ -17,59 +17,46 @@ const StatusInfo = React.memo(function StatusInfo({
       css={`
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        padding: ${3 * GU}px;
+        text-align: center;
       `}
     >
-      <div
+      <div>
+        <img
+          src={error ? errorIllustration : successIllustration}
+          width={141}
+          height={141}
+          alt=""
+        />
+      </div>
+      <h3
         css={`
-          display: flex;
-          flex-direction: column;
-          text-align: center;
+          ${textStyle('title2')};
+          margin-top: ${4 * GU}px;
         `}
       >
-        <div
-          css={`
-            align-items: center;
-          `}
-        >
-          <img
-            src={error ? errorIllustration : successIllustration}
-            width={141}
-            height={141}
-            alt=""
-          />
-        </div>
-        <span
-          css={`
-            ${textStyle('title2')};
-            margin-top: ${4 * GU}px;
-          `}
-        >
-          {title}
-        </span>
-        <span
-          css={`
-            ${textStyle('body2')};
-            color: ${theme.surfaceContentSecondary};
-            margin-top: ${1.5 * GU}px;
-          `}
-        >
-          {description}
-        </span>
+        {title}
+      </h3>
+      <span
+        css={`
+          ${textStyle('body2')};
+          color: ${theme.surfaceContentSecondary};
+          margin-top: ${1.5 * GU}px;
+        `}
+      >
+        {description}
+      </span>
 
-        {actionText && (
-          <Button
-            css={`
-              margin-top: ${3 * GU}px;
-            `}
-            mode="strong"
-            onClick={onAction}
-          >
-            {actionText}
-          </Button>
-        )}
-      </div>
+      {actionText && (
+        <Button
+          css={`
+            margin-top: ${3 * GU}px;
+          `}
+          mode="strong"
+          onClick={onAction}
+        >
+          {actionText}
+        </Button>
+      )}
     </div>
   )
 })
