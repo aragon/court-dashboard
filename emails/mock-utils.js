5eba00f3684325078de979e2221c4d584b886218
Martynas Prokopas <promaty@gmail.com>
2020-05-14 20:56:31 +0200

    migrated emails to backend

--- emails/mock-utils.js
+++
@@ -1,9 +0,0 @@
-function accountData(address) {
-  return {
-    account: address,
-    accountStart: address.slice(0, 6),
-    accountEnd: address.slice(-4),
-  }
-}
-
-module.exports = { accountData }
