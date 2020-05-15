77288f87aeeba126f9cf0e31bad3e54dcbaff7e5
Martynas Prokopas <promaty@gmail.com>
2020-05-15 11:15:00 +0100

    Emails: migrated to court-backend (#323)

--- emails/env.js
+++
@@ -1,8 +0,0 @@
-module.exports.ASSETS_URL =
-  process.env.ASSETS_URL === undefined ? './assets' : process.env.ASSETS_URL
-
-module.exports.MOCK_DATA =
-  process.env.MOCK_DATA !== undefined && process.env.MOCK_DATA !== '0'
-
-module.exports.PRINT_DATA_FOR =
-  process.env.PRINT_DATA_FOR === undefined ? null : process.env.PRINT_DATA_FOR
