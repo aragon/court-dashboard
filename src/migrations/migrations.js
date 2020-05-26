f5d8f76f0f9b417608b0a3d14283ae8c56281300
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-05-25 21:06:58 -0300

    Remove log

--- src/migrations/migrations.js
+++ src/migrations/migrations.js
@@ -3,8 +3,6 @@ import { getNetworkType } from '../lib/web3-utils'
 const ACTIVITY_KEY_REGEX = new RegExp(`^activity:${getNetworkType()}:`)
 
 export function clearActivities() {
-  console.log('clearing Activities', Object.keys(localStorage))
-
   Object.keys(localStorage)
     .filter(key => ACTIVITY_KEY_REGEX.test(key))
     .map(activityKey => localStorage.setItem(activityKey, '[]'))
