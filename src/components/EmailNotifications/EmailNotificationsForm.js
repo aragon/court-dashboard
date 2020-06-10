cd10d84bed378b8c05389bb4e5adb7be225fbec5
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-06-10 09:29:29 -0300

    Email notifications form: Remove connectedAccount prop form identity badge (#367)

--- src/components/EmailNotifications/EmailNotificationsForm.js
+++ src/components/EmailNotifications/EmailNotificationsForm.js
@@ -106,11 +106,7 @@ function EmailNotificationsForm({
           {existingEmail ? (
             <>
               <span>Enter a new email address for your account</span>
-              <IdentityBadge
-                connectedAccount={account}
-                entity={account}
-                compact
-              />
+              <IdentityBadge entity={account} compact />
               <span>
                 . We will continue sending email notifications to the current
                 email address until you verify this new email address.
@@ -119,11 +115,7 @@ function EmailNotificationsForm({
           ) : (
             <>
               <span>Associate an email address to your account </span>
-              <IdentityBadge
-                connectedAccount={account}
-                entity={account}
-                compact
-              />
+              <IdentityBadge entity={account} compact />
               <span>
                 , so you can receive notifications for all Aragon Court events.
               </span>
