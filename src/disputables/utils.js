4e402fb8b1ae00cf136325fc54f2e0dcef6401bc
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-08-31 14:51:15 -0300

    Disputable subgraphs: Remove subgraph name env var

--- src/disputables/utils.js
+++ src/disputables/utils.js
@@ -1,8 +1,6 @@
-import environment from '../environment'
 import { sanitizeNetworkType, getNetworkType } from '../lib/web3-utils'
 import { DISPUTABLE_SUBGRAPH_NAMES } from './mappings'
 
-const SUBGRAPH_NAME = environment('SUBGRAPH_NAME')
 const GRAPH_API_BASE = 'https://api.thegraph.com'
 const GRAPH_API_PATH = '/subgraphs/name/aragon/'
 
@@ -11,7 +9,5 @@ export function getSubgraphByAppId(appId) {
 
   const name = DISPUTABLE_SUBGRAPH_NAMES.get(appId)
 
-  return `${GRAPH_API_BASE}${GRAPH_API_PATH}${name}-${networkType}${
-    SUBGRAPH_NAME ? `-${SUBGRAPH_NAME}` : ''
-  }`
+  return `${GRAPH_API_BASE}${GRAPH_API_PATH}${name}-${networkType}`
 }
