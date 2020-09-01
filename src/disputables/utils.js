3dbd624f4e65e3e53c03b388a85cc46e47225c64
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-09-01 13:58:02 -0300

    Disputable subgraph urls: Provide full url by network

--- src/disputables/utils.js
+++ src/disputables/utils.js
@@ -1,13 +1,9 @@
 import { sanitizeNetworkType, getNetworkType } from '../lib/web3-utils'
-import { DISPUTABLE_SUBGRAPH_NAMES } from './mappings'
-
-const GRAPH_API_BASE = 'https://api.thegraph.com'
-const GRAPH_API_PATH = '/subgraphs/name/aragon/'
+import { DISPUTABLE_SUBGRAPH_URLS } from './mappings'
 
 export function getSubgraphByAppId(appId) {
   const networkType = sanitizeNetworkType(getNetworkType())
+  const subgraphUrls = DISPUTABLE_SUBGRAPH_URLS.get(appId)
 
-  const name = DISPUTABLE_SUBGRAPH_NAMES.get(appId)
-
-  return `${GRAPH_API_BASE}${GRAPH_API_PATH}${name}-${networkType}-staging` // TODO: Remove when dvoting subgraph deployed to main subgraph.
+  return subgraphUrls ? subgraphUrls[networkType] : ''
 }
